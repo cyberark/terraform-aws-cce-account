2c457073814c392018bf0faaee55ee7e9c00525e
CyberarkReleaser <svc_go_ark_releaser@cyberark.com>
2026-05-14 10:21:28 +0000

    CRT-14050: fix: fix type issue: Merge pull request #22 from Everest/CRT-14050

--- main.tf
+++ main.tf
@@ -23,7 +23,7 @@ module "sia" {
   source         = "./modules/sia"
   sia_account_id = data.idsec_cce_aws_tenant_service_details.get_tenant_data.services_details.dpa.service_account_id
   tenant_id      = data.idsec_cce_aws_tenant_service_details.get_tenant_data.tenant_id
-  count          = var.sia.enable != false ? 1 : 0
+  count          = var.sia.enable ? 1 : 0
 }
 
 # SCA (Secure Cloud Access) Module
@@ -32,35 +32,31 @@ module "sca" {
   sca_service_account_id = data.idsec_cce_aws_tenant_service_details.get_tenant_data.services_details.sca.service_account_id
   tenant_id              = data.idsec_cce_aws_tenant_service_details.get_tenant_data.tenant_id
   custom_role_name       = var.sca.role_name
-  count                  = var.sca.enable != false ? 1 : 0
+  count                  = var.sca.enable ? 1 : 0
+}
+
+locals {
+  services_list = flatten([
+    var.sia.enable ? [{
+      service_name = "dpa"
+      resources    = { DpaRoleArn = module.sia[0].deployed_resources.main }
+    }] : [],
+
+    var.sca.enable ? [{
+      service_name = "sca"
+      resources = {
+        scaPowerRoleArn = module.sca[0].deployed_resources.main
+      }
+    }] : []
+  ])
 }
 
 # Register AWS account with CyberArk CCE
 resource "idsec_cce_aws_account" "add_account" {
-  count                = var.sca.enable == true || var.sia.enable == true ? 1 : 0
+  count                = var.sca.enable || var.sia.enable ? 1 : 0
   account_id           = var.account_id
   account_display_name = var.account_display_name
   deployment_region    = data.aws_region.current.name
 
-  services = concat(
-    # Add SIA service if enabled
-    var.sia.enable != false ? [
-      {
-        service_name = "dpa"
-        resources = {
-          "DpaRoleArn" = module.sia[0].deployed_resources.main
-        }
-      }
-    ] : [],
-
-    # Add SCA service if enabled
-    var.sca.enable != false ? [
-      {
-        service_name = "sca"
-        resources = {
-          "scaPowerRoleArn" = module.sca[0].deployed_resources.main
-        }
-      }
-    ] : []
-  )
+  services = local.services_list
 }
